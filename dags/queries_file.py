2c42ff3c10c878f3c69e9d3ae8226bc344121c76
Mitch Jablonski <mitchjabdota@gmail.com>
2021-03-21 20:41:36 -0400

    fixes variable mismatch between insert and create

--- dags/queries_file.py
+++ dags/queries_file.py
@@ -121,7 +121,7 @@ create_time_table = """CREATE TABLE public."time" (
     week int4,
     "month" varchar(256),
     "year" int4,
-    weekday varchar(256),
+    dayofweek varchar(256),
     CONSTRAINT time_pkey PRIMARY KEY (start_time)
 );"""
 
